af5bc49b9ed37f1c09c991567636f6a2a688b0e9
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-05 19:43:51 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- memoization/climbing-stairs.cpp
+++ memoization/climbing-stairs.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int solve(int i, int n){
+    int solve(int i, int n, vector<int> &dp){
         if(i==n) return 1;
         if(i>n) return 0;
+        if(dp[i]!=-1) return dp[i];
 
-        int a = solve(i+1,n);
-        int b = solve(i+2,n);
-        return a+b;
+        int a = solve(i+1,n,dp);
+        int b = solve(i+2,n,dp);
+        dp[i] = a+b;
+        return dp[i];
     }
     int climbStairs(int n) {
-        return solve(0,n);
+        vector<int> dp(n,-1);
+        return solve(0,n,dp);
     }
 };
