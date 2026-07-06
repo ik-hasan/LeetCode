dc7fbcaa8cbf56f6dd767160ad4f9874783812bd
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-07 01:38:26 +0530

    LC-lemonade-change [Runtime: 0 ms; Memory: 87.2 MB]

--- array/lemonade-change.cpp
+++ array/lemonade-change.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        unordered_map<int,int> mp;
-        for(auto it:bills) mp[it]++;
+        int five = 0, ten = 0;
 
-        int five = mp[5];
-        int ten = mp[10];
-        int twenty = mp[20];
+        for (int bill : bills) {
 
-        bool ans = false;
-        if(five>=ten) {
-            five-=ten;
+            if (bill == 5) {
+                five++;
+            }
+
+            else if (bill == 10) {
+                if (five == 0)
+                    return false;
+
+                five--;
+                ten++;
+            }
+
+            else { // bill == 20
+
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
         }
-        else return false;
 
-        if((twenty<=ten && twenty<=five) || 3*twenty<=five) ans = true;
-        return ans;
+        return true;
     }
 };
