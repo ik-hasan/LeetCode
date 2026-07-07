fbf7d2dc500f8bfcbb5829039fb526bd013196df
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-07 23:28:45 +0530

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- greedy/jump-game-ii.cpp
+++ greedy/jump-game-ii.cpp
@@ -1,79 +1,18 @@
 class Solution {
 public:
-    //hmara logic shi h bs DP lga kr Optimize krna h fir TLE nhi marega
-    // int solve(vector<int> nums,int index,int result){
-    //     if(index == nums.size()-1) return result;
-    //     if(index >= nums.size()) return INT_MAX;
-
-    //     int ans = INT_MAX;
-    //     for(int i=1;i<=nums[index];i++){
-    //         ans = min( ans,solve(nums,index+i,result+1) );
-    //     }
-    //     return ans;
-    // }
-    // int jump(vector<int>& nums) {
-    //     int index = 0,result = 0;
-    //     return solve(nums,index,result);
-    // }
-
-
-    //DP SOLN OF ABOVE CODE
-    // int solvedp(vector<int> &nums,int index,int result,vector<vector<int>> &dp_arr){
-    //     if(index == nums.size()-1) return result;
-    //     if(index >= nums.size()) return INT_MAX;
-
-    //     if(dp_arr[index][result] != -1){
-    //         return dp_arr[index][result];
-    //     }
-
-    //     int ans = INT_MAX;
-    //     for(int i=1;i<=nums[index];i++){
-    //         ans = min( ans,solvedp(nums,index+i,result+1,dp_arr) );
-    //     }
-
-    //     dp_arr[index][result]=ans;
-    //     return dp_arr[index][result];
-    // }
-    // int jump(vector<int>& nums) {
-    //     int index = 0,result = 0;
-    //     int n = nums.size();
-
-    //     //STEP-1 CREATE AND PASS DP_ARR
-    //     vector<vector<int>> dp_arr(n+1,vector<int>(n+1,-1));
-    //     // vector<int> dp_arr(n+1,-1);
-    //     return solvedp(nums,index,result,dp_arr);
-    // }
-    
-
-
-
-
-
-
-
-
-
-
-
-
-    int solve(vector<int>& nums, int index,vector<int> &dp){
-        if(index >= nums.size()) return INT_MAX;
-        if(index==nums.size()-1){
-            return 0;
+    int solve(int i,vector<int>& nums){
+        if(i==nums.size()-1) return 1;
+        if(i>=nums.size()) return INT_MAX-1;
+        int ans = 0;
+        int step = INT_MAX;
+        for(int j=1;j<=nums.size();j++){
+            ans = 1 + solve(i+j,nums);
+            step = min(step,ans);
         }
-        if(dp[index]!=-1) return dp[index];
-        int ans = INT_MAX;
-        for(int i=1;i<=nums[index];i++){
-            int temp = solve(nums,index+i,dp);
-            if(temp!=INT_MAX) ans = min( ans, 1 + temp);
-        }
-        dp[index]=ans;
-        return dp[index];
+        return step;
     }
-    int jump(vector<int>& nums){
-        vector<int> dp(nums.size(),-1);
-        return solve(nums,0,dp);
+    int jump(vector<int>& nums) {
+        return solve(0,nums);
     }
-
-
 };
+
