6bdffdae0ba93f0e88917f0a17994280f71955b6
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-09 00:05:45 +0530

    LC-non-overlapping-intervals [Runtime: 43 ms; Memory: 94 MB]

--- sorting/non-overlapping-intervals.cpp
+++ sorting/non-overlapping-intervals.cpp
@@ -1,15 +1,34 @@
+// class Solution {
+// public:
+//     int eraseOverlapIntervals(vector<vector<int>>& intr) {
+//         sort(intr.begin(),intr.end());
+
+//         int checker = INT_MIN, ans= 0;
+//         for(auto it:intr){
+//             int st = it[0];
+//             int end = it[1];
+//             if(st>=checker){
+//                 checker = end;
+//             }
+//             else ans++;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intr) {
-        sort(intr.begin(),intr.end());
 
-        int checker = INT_MIN, ans= 0;
-        for(auto it:intr){
+        sort(intr.begin(), intr.end(), [](auto &a, auto &b){
+            return a[1] < b[1];
+        });
+        int ans = 0, checker = INT_MIN;
+
+        for(auto &it : intr){
             int st = it[0];
             int end = it[1];
-            if(st>=checker){
-                checker = end;
-            }
+            if(st >= checker) checker = end;
             else ans++;
         }
         return ans;
