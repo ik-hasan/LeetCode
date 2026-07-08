1380e75f51945f9b1a057150f29040a9bbbc6aed
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-08 18:24:11 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/unique-paths-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    int solve(int i, int j, vector<vector<int>>& obs){
+        if(i==obs.size()-1 && j==obs[0].size()-1) return 1;
+        if(i>=obs.size() || j>=obs[0].size()) return 0;
+        if(obs[i][j]==1) return 0;
+
+        int right = solve(i,j+1,obs);
+        int down = solve(i+1,j,obs);
+        return right + down;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        return solve(0,0,obstacleGrid);
+    }
+};
