eb71b6a14461d8a0b40bf4d880c30c6caa59d172
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-11 13:04:40 +0530

    LC-cherry-pickup-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/cherry-pickup-ii.cpp
+++ dynamic-programming/cherry-pickup-ii.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
 
-    int solve(int row, int col1, int col2, vector<vector<int>>& grid){
+    int solve(int row, int col1, int col2, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp){
         
         int n = grid.size();
         int m = grid[0].size();
         if(row>=n || row<0 || col1>=m || col1<0 || col2>=m || col2<0) return -1e9;
+        if(dp[row][col1][col2]!=-1) return dp[row][col1][col2];
 
         int totalCherry = 0;
         if(col1==col2) totalCherry = grid[row][col1];
@@ -14,13 +15,17 @@ public:
         int ans = 0;
         for(int i=-1;i<=1;i++){
             for(int j=-1;j<=1;j++){
-                ans = max(ans,solve(row+1,col1+i,col2+j,grid));
+                ans = max(ans,solve(row+1,col1+i,col2+j,grid,dp));
             }
         }
-        return totalCherry+ans;
+        dp[row][col1][col2] = totalCherry+ans;
+        return dp[row][col1][col2];
     }
 
     int cherryPickup(vector<vector<int>>& grid) {
-        return solve(0,0,grid[0].size()-1,grid);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        return solve(0,0,grid[0].size()-1,grid,dp);
     }
 };
