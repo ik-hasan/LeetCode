6b913faa0faf6702846a13458fb81540c017b2f5
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-12 18:41:13 +0530

    LC-partition-equal-subset-sum [Runtime: N/A; Memory: N/A]

--- array/partition-equal-subset-sum.cpp
+++ array/partition-equal-subset-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
 
-    bool solve(int i, int sum, vector<int>& nums){
+    bool solve(int i, int sum, vector<int>& nums, vector<vector<int>> &dp){
         if(i>=nums.size() || sum<0) return false;
         if(sum==0) return true;
+        if(dp[i][sum] != 0) return dp[i][sum];
 
-        return solve(i+1,sum,nums) || solve(i+1,sum-nums[i],nums);
+        dp[i][sum] = solve(i+1,sum,nums,dp) || solve(i+1,sum-nums[i],nums,dp);
+        return dp[i][sum];
     }
     bool canPartition(vector<int>& nums) {
         int totalSum = accumulate(nums.begin(),nums.end(),0);
         if(totalSum%2) return false;
         int sum = totalSum/2;
+        vector<vector<int>> dp(nums.size()+1,vector<int>((totalSum/2)+1,0));
 
-        return solve(0,sum,nums);
+        return solve(0,sum,nums,dp);
     }
 };
