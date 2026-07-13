d28233c0e243072d7cf72361a9b526de99d2626d
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-14 00:01:17 +0530

    LC-valid-parenthesis-string [Runtime: N/A; Memory: N/A]

--- string/valid-parenthesis-string.cpp
+++ string/valid-parenthesis-string.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    bool solve(int open, int i, string &s){
+    bool solve(int open, int i, string &s, vector<vector<int>> &dp){
         if(open < 0) return false;
-
         if(i == s.size()) return open == 0;
+        if(dp[i][open]!=0) return dp[i][open];
 
         bool a=false,b=false,c=false;
-        if(s[i]=='(') a = solve(open+1,i+1,s);
-        else if(s[i]==')') b = solve(open-1, i+1,s);
+        if(s[i]=='(') a = solve(open+1,i+1,s,dp);
+        else if(s[i]==')') b = solve(open-1, i+1,s,dp);
         else{
-            c = solve(open+1,i+1,s) || //'('
-            solve(open-1,i+1,s) || //')'
-            solve(open,i+1,s); //empty string
+            c = solve(open+1,i+1,s,dp) || //'('
+            solve(open-1,i+1,s,dp) || //')'
+            solve(open,i+1,s,dp); //empty string
         }
-        return a || b || c;
+        dp[i][open] = a || b || c;
+        return dp[i][open];
     }
     bool checkValidString(string s) {
         int open = 0;
-        return solve(open,0,s);
+        vector<vector<int>> dp(s.size(),vector<int>(s.size(),0));
+        return solve(open,0,s,dp);
     }
 };
