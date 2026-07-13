3563fe1195df5ac6b121f0d125bdbff03aae9318
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-14 00:03:07 +0530

    LC-valid-parenthesis-string [Runtime: N/A; Memory: N/A]

--- stack/valid-parenthesis-string.cpp
+++ stack/valid-parenthesis-string.cpp
@@ -3,7 +3,7 @@ public:
     bool solve(int open, int i, string &s, vector<vector<int>> &dp){
         if(open < 0) return false;
         if(i == s.size()) return open == 0;
-        if(dp[i][open]!=0) return dp[i][open];
+        if(dp[i][open]!=-1) return dp[i][open];
 
         bool a=false,b=false,c=false;
         if(s[i]=='(') a = solve(open+1,i+1,s,dp);
@@ -18,7 +18,7 @@ public:
     }
     bool checkValidString(string s) {
         int open = 0;
-        vector<vector<int>> dp(s.size(),vector<int>(s.size(),0));
+        vector<vector<int>> dp(s.size(),vector<int>(s.size(),-1));
         return solve(open,0,s,dp);
     }
 };
