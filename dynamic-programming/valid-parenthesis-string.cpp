c62c9c901a5ad79dad4fec3995fc30d400dddbf2
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-13 23:03:26 +0530

    LC-valid-parenthesis-string [Runtime: N/A; Memory: N/A]

--- dynamic-programming/valid-parenthesis-string.cpp
+++ dynamic-programming/valid-parenthesis-string.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int open=0;
+        int open=0,star=0,close=0;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='(') open++;
+        //     else if(s[i]==')') close++;
+        //     else star++;
+        // }
+        // cout<<open<<" "<<close<<" "<<star;
         for(int i=0;i<s.size();i++){
-            if(open<0) return false;
             if(s[i]=='(') open++;
             else if(s[i]==')') open--;
+            else star++;
         }
-        if(open>0) return false;
-        return true;
+        if(open==0) return true;
+        if(abs(open)<=star) return true;
+        return false;
     }
 };
