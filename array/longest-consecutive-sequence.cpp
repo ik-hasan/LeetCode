dbd3d5ba76fb618fa6c1b60b5362bdc93bacf5ea
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-18 01:53:12 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- array/longest-consecutive-sequence.cpp
+++ array/longest-consecutive-sequence.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    // int lowest(vector<int> nums){
-    //     if(nums.size()==0) 0;
-    //     if(nums.size()==1) return nums[0];
-    //     int minn=nums[0];
-    //     for(int i=1;i<nums.size();i++){
-    //         if(minn>nums[i]){
-    //             minn=nums[i];
-    //         }
-    //     }
-    //     return minn;
-    // }
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0) return 0;
-        // if(nums.size()==1) return 1;
-        sort(nums.begin(),nums.end());
-        int count=1,ans=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]) continue;
-            if(nums[i]==nums[i-1]+1){
-                count++;
+
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        int ans = 0;
+
+        for (int num : st) {
+
+            // sequence starts here
+            if (st.find(num - 1) == st.end()) {
+
+                int curr = num;
+                int len = 1;
+
+                while (st.find(curr + 1) != st.end()) {
+                    curr++;
+                    len++;
+                }
+
+                ans = max(ans, len);
             }
-            else{
-                count=1;
-            }
-            ans =max(ans,count);
         }
+
         return ans;
     }
-};
+}
+;
