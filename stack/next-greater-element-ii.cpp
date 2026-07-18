ceea8e11e7ec0b6f6d5ff70cffa47469acf3f8e6
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-18 15:56:46 +0530

    LC-next-greater-element-ii [Runtime: N/A; Memory: N/A]

---
+++ stack/next-greater-element-ii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> v(nums.begin(),nums.end());
+        for(auto it:nums) v.push_back(it);
+
+
+        
+    }
+};
