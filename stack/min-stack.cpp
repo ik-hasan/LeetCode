5cc6572aba61533248f2a2fb62db82900dfbbdef
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-18 15:18:56 +0530

    LC-min-stack [Runtime: N/A; Memory: N/A]

--- stack/min-stack.cpp
+++ stack/min-stack.cpp
@@ -1,38 +1,23 @@
 class MinStack {
 public:
-    MinStack() {
-        
-    }
-    int minm = INT_MAX;
-    int index = -1;
     vector<pair<int,int>> v;
-    void push(int value) {
-        index++;
-        minm = min(minm,value);
-        v.push_back({value,minm});
+
+    void push(int val) {
+        if(v.empty())
+            v.push_back({val,val});
+        else
+            v.push_back({val,min(val,v.back().second)});
     }
-    
+
     void pop() {
-        if(index >= 0){
-            v.pop_back();
-            index--;
-        }
+        v.pop_back();
     }
-    
+
     int top() {
-        return (index>=0) ? v[index].first : -1;
+        return v.back().first;
     }
-    
+
     int getMin() {
-        return v[index].second;
+        return v.back().second;
     }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(value);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
