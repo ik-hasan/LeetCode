dbc28b47dbafcfb79f0e94636e5cae7db4bebd8a
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-18 16:22:33 +0530

    LC-next-greater-element-ii [Runtime: N/A; Memory: N/A]

--- array/next-greater-element-ii.cpp
+++ array/next-greater-element-ii.cpp
@@ -3,8 +3,37 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         vector<int> v(nums.begin(),nums.end());
         for(auto it:nums) v.push_back(it);
+        // for(auto it:v) cout<<it<<" ";
 
+        stack<int> st;
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        for(int i=v.size()-1;i>=0;i--){
+            if(st.size()==0) {
+                mp[i] = -1;
+                st.push(v[i]);
+                continue;
+            }
+            
+            bool flag = true;
+            while(v[i]>=st.top()){
+                st.pop();
+                if(st.empty()){
+                    mp[i] = -1;
+                    st.push(v[i]);
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                mp[i] = st.top();
+                st.push(v[i]);
+            }
+        }
 
-        
+        for(int i=0;i<nums.size();i++){
+            ans.push_back(mp[i]);
+        }
+        return ans;
     }
 };
