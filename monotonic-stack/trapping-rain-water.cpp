052b830e48eb6a5ad18f9334e5870b77699861ef
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-18 18:47:03 +0530

    LC-trapping-rain-water [Runtime: N/A; Memory: N/A]

--- monotonic-stack/trapping-rain-water.cpp
+++ monotonic-stack/trapping-rain-water.cpp
@@ -1,29 +1,70 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+
+//         int n = height.size();
+//         if(n == 0) return 0;
+
+//         vector<int> leftMax(n);
+//         vector<int> rightMax(n);
+
+//         // Left maximum
+//         leftMax[0] = height[0];
+//         for(int i = 1; i < n; i++){
+//             leftMax[i] = max(leftMax[i-1], height[i]);
+//         }
+
+//         // Right maximum
+//         rightMax[n-1] = height[n-1];
+//         for(int i = n-2; i >= 0; i--){
+//             rightMax[i] = max(rightMax[i+1], height[i]);
+//         }
+
+//         int ans = 0;
+
+//         for(int i = 0; i < n; i++){
+//             ans += min(leftMax[i], rightMax[i]) - height[i];
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int trap(vector<int>& height) {
 
         int n = height.size();
-        if(n == 0) return 0;
 
-        vector<int> leftMax(n);
-        vector<int> rightMax(n);
+        int left = 0;
+        int right = n - 1;
 
-        // Left maximum
-        leftMax[0] = height[0];
-        for(int i = 1; i < n; i++){
-            leftMax[i] = max(leftMax[i-1], height[i]);
-        }
-
-        // Right maximum
-        rightMax[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--){
-            rightMax[i] = max(rightMax[i+1], height[i]);
-        }
+        int leftMax = 0;
+        int rightMax = 0;
 
         int ans = 0;
 
-        for(int i = 0; i < n; i++){
-            ans += min(leftMax[i], rightMax[i]) - height[i];
+        while(left < right) {
+
+            if(height[left] <= height[right]) {
+
+                if(height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    ans += leftMax - height[left];
+
+                left++;
+            }
+            else {
+
+                if(height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    ans += rightMax - height[right];
+
+                right--;
+            }
         }
 
         return ans;
