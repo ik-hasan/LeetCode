0f64f8bd0ee501a42e784473931082dcf19cfb35
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-20 17:13:49 +0530

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array.cpp
+++ array/search-in-rotated-sorted-array.cpp
@@ -1,65 +1,23 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int st = 0,end = nums.size()-1;
+        int s=0,e=nums.size()-1;
 
-        while(st<=end){
-            int mid = (st+end)/2;
+        while(s<=e){
+            int mid = s+(e-s)/2;
 
             if(nums[mid]==target) return mid;
 
-            //left half sorted
-            if(nums[st]<=nums[mid]){
-                if(nums[st]<=target && nums[mid]>target){
-                    end = mid-1;
-                }
-                else st=mid+1;
+            if(nums[s]<=nums[mid]){
+                if(nums[s]<=target && target<nums[mid]) e=mid-1;
+                else s=mid+1;
             }
-
-            //right half sorted
-            else if(nums[st]>nums[mid]){
-                if(target<=nums[end] && target>nums[mid]){
-                    st = mid+1;
-                }
-                else end = mid-1;
+            else{
+                if(target<=nums[e] && nums[mid]<target){
+                    s=mid+1;
+                }else e=mid-1;
             }
         }
         return -1;
     }
 };
-
-
-// class Solution {
-// public:
-//     int search(vector<int>& nums, int target) {
-
-//         int st = 0, end = nums.size()-1;
-
-//         while(st <= end){
-
-//             int mid = st + (end-st)/2;
-
-//             if(nums[mid] == target) return mid;
-
-//             // left half sorted
-//             if(nums[st] <= nums[mid]){
-
-//                 if(nums[st] <= target && target < nums[mid])
-//                     end = mid - 1;
-//                 else
-//                     st = mid + 1;
-//             }
-
-//             // right half sorted
-//             else{
-
-//                 if(nums[mid] < target && target <= nums[end])
-//                     st = mid + 1;
-//                 else
-//                     end = mid - 1;
-//             }
-//         }
-
-//         return -1;
-//     }
-// };
