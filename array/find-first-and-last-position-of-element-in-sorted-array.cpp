9a19c824cced71473c2088fd16300d42a9d8647e
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-20 11:43:04 +0530

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: N/A; Memory: N/A]

--- array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans{-1,-1};
-        int st = 0, end = nums.size()-1;
+        int a=-1,b=-1,s=0,e=nums.size()-1;
 
-        // FIRST OCCURRENCE
-        while(st<=end){
-            int mid = st + (end-st)/2;   
-            if(nums[mid] < target) st = mid+1;
-            else if(nums[mid] > target) end = mid-1;
-            else{
-                ans[0] = mid;           
-                end = mid-1;//move left to find later index           
-            }
-        }
-        st = 0;                         
-        end = nums.size()-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            int midd=nums[mid];
 
-        // LAST OCCURRENCE
-        while(st<=end){
-            int mid = st + (end-st)/2;   
-            if(nums[mid] < target) st = mid+1;
-            else if(nums[mid] > target) end = mid-1;
-            else{
-                ans[1] = mid;           
-                st = mid+1;//move right to find later index
+            if(midd==target){
+                a=mid;
+                e=mid-1;
             }
+            else if(midd>target) e=mid-1;
+            else s=mid+1;
         }
-        return ans;                   
+
+        s=0,e=nums.size()-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            int midd=nums[mid];
+
+            if(midd==target){
+                b=mid;
+                s=mid+1;
+            }
+            else if(midd>target) e=mid-1;
+            else s=mid+1;
+        }
+        return {a,b};
     }
 };
