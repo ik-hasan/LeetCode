78ab1f9181e13687f740df6944795fd0dc173e70
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-07-25 11:37:15 +0530

    LC-best-time-to-buy-and-sell-stock-using-strategy [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -0,0 +1,37 @@
+typedef long long ll;
+
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        int n = prices.size();
+
+        ll orignal = 0;
+        for (int i = 0; i < n; i++) {
+            orignal += strategy[i] * prices[i];
+        }
+        int half = k / 2;
+        ll change = 0;
+
+        //first window calculation
+        for (int i = 0; i < half; i++) {
+            change += (0 * prices[i]) - (strategy[i] * prices[i]);
+        }
+        for (int i = half; i < k; i++) {
+            change += (1 * prices[i]) - (strategy[i] * prices[i]);
+        }
+
+        long long maxi = change;
+
+        //remaining window calculation
+        for (int i = 1; i <= n - k; i++) {
+            ll lefti = 0 * prices[i-1] - (strategy[i - 1] * prices[i - 1]);
+            ll midi = 0 * prices[i+half-1] - strategy[i + half - 1] * prices[i + half - 1];
+            ll righti = 1 * prices[i + k - 1] - strategy[i + k - 1] * prices[i + k - 1];
+
+            change = change - lefti + midi + righti;
+
+            maxi = max(maxi, change);
+        }
+        return orignal + max(0LL, maxi);
+    }
+};
