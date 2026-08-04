3e3a2ec371b16548e370f8cc581e36386c6bb731
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-08-04 21:17:00 +0530

    LC-find-missing-elements [Runtime: N/A; Memory: N/A]

--- hash-table/find-missing-elements.cpp
+++ hash-table/find-missing-elements.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        
+        sort(nums.begin(),nums.end());
+        vector<int> ans;
+
+        int n = nums[nums.size()-1] - nums[0] + 1;
+
+        for(int i = 0;i<n-1;i++){
+            if(nums[i+1]!=nums[i]+1) {
+                ans.push_back(nums[i]+1);
+                nums.insert(nums.begin() + i+1,nums[i]+1);
+            }
+        }
+        return ans;
     }
 };
