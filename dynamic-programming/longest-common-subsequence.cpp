3fe207ca6d2071ba38fe399df4b6a768b9431b93
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-08-17 23:12:02 +0530

    LC-longest-common-subsequence [Runtime: 13 ms; Memory: 9 MB]

--- dynamic-programming/longest-common-subsequence.cpp
+++ dynamic-programming/longest-common-subsequence.cpp
@@ -45,6 +45,34 @@
 
 
 //tab
+// class Solution {
+// public:
+
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n = text1.size();
+//         int m = text2.size();
+//         vector<vector<int>> dp(
+//             n+1,
+//             vector<int>(m+1,0)
+//         );
+//         for(int i=n-1;i>=0;i--){
+//             for(int j=m-1;j>=0;j--){
+//                 if(text1[i]==text2[j])
+//                     dp[i][j] = 1+dp[i+1][j+1];
+//                 else
+//                     dp[i][j] = max(
+//                         dp[i+1][j],
+//                         dp[i][j+1]
+//                     );
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+
+
+//so
 class Solution {
 public:
 
@@ -52,28 +80,21 @@ public:
 
         int n = text1.size();
         int m = text2.size();
-
-        vector<vector<int>> dp(
-            n+1,
-            vector<int>(m+1,0)
-        );
+        vector<int> next(m+1,0);
+        vector<int> curr(m+1,0);
 
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-
                 if(text1[i]==text2[j])
-                    dp[i][j] = 1+dp[i+1][j+1];
-
+                    curr[j] = 1+next[j+1];
                 else
-                    dp[i][j] = max(
-                        dp[i+1][j],
-                        dp[i][j+1]
+                    curr[j] = max(
+                        next[j],
+                        curr[j+1]
                     );
             }
+            next = curr;
         }
-
-        return dp[0][0];
+        return next[0];
     }
 };
-
-
