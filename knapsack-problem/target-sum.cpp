d962400f5ee75dbb287f2db55e6f0b835d6e69da
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-08-17 22:35:54 +0530

    LC-target-sum [Runtime: 1002 ms; Memory: 11.5 MB]

--- knapsack-problem/target-sum.cpp
+++ knapsack-problem/target-sum.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
     int solve(int i, int sum, vector<int>& nums, int target) {
-        if (i == nums.size()) {
-            return (sum == target);
-        }
+        if(i==nums.size() && sum==target) return 1;
+        if(i>=nums.size()) return 0;
 
-        if (dp[i][sum + 1000] != -1)
-            return dp[i][sum + 1000];
-
-        int plus = solve(i + 1, sum + nums[i], nums, target);
-        int minus = solve(i + 1, sum - nums[i], nums, target);
-
-        return dp[i][sum + 1000] = plus + minus;
+        int add = solve(i+1,sum+nums[i],nums,target);
+        int sub = solve(i+1,sum-nums[i],nums,target);
+        return add+sub;
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        dp.assign(nums.size(), vector<int>(2001, -1));
-
-        return solve(0, 0, nums, target);
+        return solve(0,0,nums,target);
     }
 };
