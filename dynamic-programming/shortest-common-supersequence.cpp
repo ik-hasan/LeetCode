e730d188397a32c4a4f41f883a1451a9a9d605f3
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-08-26 15:18:13 +0530

    LC-shortest-common-supersequence [Runtime: N/A; Memory: N/A]

--- dynamic-programming/shortest-common-supersequence.cpp
+++ dynamic-programming/shortest-common-supersequence.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
 
-    string solve(int i, int j, string &s1, string &s2){
+    string solve(int i, int j, string &s1, string &s2, vector<vector<string>> &dp){
         if(i<=s1.length()-1 && j>s2.length()-1) return s1.substr(i,s1.length()-i);
         if(j<=s2.length()-1 && i>s1.length()-1) return s2.substr(j,s2.length()-j);
         if(i>=s1.length() || j>=s2.length()) return "";
-        if(s1[i]==s2[j]) return s1[i] + solve(i+1,j+1,s1,s2);
 
-        string a = s1[i] + solve(i+1,j,s1,s2);
-        string b = s2[j] + solve(i,j+1,s1,s2);
+        if(dp[i][j]!= "$") return dp[i][j];
+        if(s1[i]==s2[j]) return dp[i][j] = s1[i] + solve(i+1,j+1,s1,s2,dp);
 
-        return (a.length()>b.length()) ? b : a;
+        string a = s1[i] + solve(i+1,j,s1,s2,dp);
+        string b = s2[j] + solve(i,j+1,s1,s2,dp);
+
+        return dp[i][j] = (a.length()>b.length()) ? b : a;
     }
     string shortestCommonSupersequence(string str1, string str2) {
-        return solve(0,0,str1,str2);
+        vector<vector<string>> dp(str1.length(),vector<string>(str2.length(),"$"));
+        return solve(0,0,str1,str2,dp);
     }
 };
