13dea0cc47618725cadf32b1390f342631faf9c1
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-08-26 12:30:51 +0530

    LC-minimum-insertion-steps-to-make-a-string-palindrome [Runtime: N/A; Memory: N/A]

--- string/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ string/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
 
-    int solve(int st, int end, string & s){
+    int solve(int st, int end, string & s, vector<vector<int>> &dp){
         if(st>=end) return 0;
-        if(s[st]==s[end]) return solve(st+1,end-1,s);
-        return 1 + min(solve(st+1,end,s), solve(st,end-1,s));
+        if(dp[st][end]!=-1) return dp[st][end];
+        if(s[st]==s[end]) return dp[st][end] = solve(st+1,end-1,s,dp);
+        return dp[st][end] = 1 + min(solve(st+1,end,s,dp), solve(st,end-1,s,dp));
     }
     int minInsertions(string s) {
         int n = s.length();
-        return solve(0,n-1,s);
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        return solve(0,n-1,s,dp);
     }
 };
