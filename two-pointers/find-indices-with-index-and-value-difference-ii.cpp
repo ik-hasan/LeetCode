faf6c556da990569d3bd470b48a0847842c34513
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-09-01 16:26:17 +0530

    LC-find-indices-with-index-and-value-difference-ii [Runtime: N/A; Memory: N/A]

--- two-pointers/find-indices-with-index-and-value-difference-ii.cpp
+++ two-pointers/find-indices-with-index-and-value-difference-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
-        
+    vector<int> findIndices(vector<int>& nums, int id, int vd) {
+        int minIndex =0,maxIndex=0;
+        for(int i=id;i<nums.size();i++){
+            int j = i-id;
 
-        for(int i=0;i<nums.size();i++){
-            int j = indexDifference + i;
-            while(j<nums.size()){
-                 if( abs(nums[i] - nums[j]) >= valueDifference) {
-                    return {i,j};
-                }
-                j++;
-            }
-        }
+            if(nums[j] < nums[i]) minIndex = j;
+            if(nums[j] > nums[i]) maxIndex = j;
+
+            if(abs(nums[i]-nums[minIndex]) >= vd) return {minIndex,i}; 
+            if(abs(nums[i]-nums[maxIndex]) >= vd) return {maxIndex,i}; 
+        }   
         return {-1,-1};
     }
 };
