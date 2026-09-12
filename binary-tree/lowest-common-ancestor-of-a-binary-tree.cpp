a653fc56e2f464ab17d400512ac8ca9cb836d84c
IKRAMUL HASAN <ikramul7388@gmail.com>
2026-09-12 15:49:04 +0530

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: N/A; Memory: N/A]

--- binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -11,15 +11,17 @@ class Solution {
 public:
 
     TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(root==NULL) return NULL;
-        if(root->val==p->val) return p;
-        if(root->val==q->val) return q;
+        if(root == NULL) return NULL;
 
-        auto a = solve(root->left,p,q);
-        auto b = solve(root->right,p,q);
-        if(a!=NULL || b!=NULL) return root;
-        // if(b!=NULL) return b;
-        return NULL;
+        if(root == p) return p;
+        if(root == q) return q;
+
+        auto a = solve(root->left, p, q);
+        auto b = solve(root->right, p, q);
+
+        if(a != NULL && b != NULL) return root;
+        else if(a != NULL && b == NULL) return a;
+        else return b;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root->val==p->val) return p;
